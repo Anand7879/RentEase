71609ca07c839f232b1da62762f6daade6b00b82
Anand7879 <Anand7879@users.noreply.github.com>
2026-03-08 09:25:06 +0530

    fix cors for multipart form data

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/(.*)", cors(corsOptions)); // ← Preflight requests handle karo
 
 app.use(express.json());
 app.use(cookieParser());
